d90bdef5a91132099291b16b46bba8ea689dc3d5
Tushar Verma <55954950+vtushar2@users.noreply.github.com>
2021-06-02 08:50:05 +0530

    Create OddOrEven.java

---
+++ School level/OddOrEven.java
@@ -0,0 +1,13 @@
+class Solution{
+    static String oddEven(int N){
+        // code here
+        if(N%2==0)
+        {
+            return "even";
+        }
+        else
+        {
+            return "odd";
+        }
+    }
+}
