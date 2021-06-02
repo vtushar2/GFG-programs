e57f3868163ec4fb5ad0658ac29036d7e950b007
Tushar Verma <55954950+vtushar2@users.noreply.github.com>
2021-06-02 15:55:53 +0530

    Create JavaClassesIntroduction.java

---
+++ School level/JavaClassesIntroduction.java
@@ -0,0 +1,25 @@
+class task
+{
+    int l,w,h;
+    
+    public void set_length(int l)
+    {
+        //Add your code here.
+        this.l=l;
+    }
+    public void set_width(int w)
+    {
+        //Add your code here.4
+        this.w=w;
+    }
+    public void set_height(int h)
+    {
+        //Add your code here.
+        this.h=h;
+    }
+    public void volume()
+    {
+        //Add your code here.
+        System.out.println(l*w*h);
+    }
+}
