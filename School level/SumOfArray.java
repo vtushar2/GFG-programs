fb690ff6fe46f5c3cab988cbcc4328f30928defb
Tushar Verma <55954950+vtushar2@users.noreply.github.com>
2021-06-02 22:07:26 +0530

    Create SumOfArray.java

---
+++ School level/SumOfArray.java
@@ -0,0 +1,11 @@
+class Solution {
+    int sum(int arr[], int n) {
+        // code here
+        int sum=0;
+        for(int i=0;i<n;i++)
+        {
+            sum+=arr[i];
+        }
+        return sum;
+    }
+}
