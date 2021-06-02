3f614a5ee30ab05c99c38ef2914475ec6a8264fc
Tushar Verma <55954950+vtushar2@users.noreply.github.com>
2021-06-02 08:56:49 +0530

    Create CountOfCamelCaseCharacters.java

---
+++ School level/CountOfCamelCaseCharacters.java
@@ -0,0 +1,16 @@
+class Sol
+{
+    int countCamelCase (String s)
+    {
+        // your code here  
+        int count=0;
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)>='A' && s.charAt(i)<='Z')
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
