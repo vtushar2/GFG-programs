bc90754e01b04b3423a8bfd62e103df2b58c0ca0
Tushar Verma <55954950+vtushar2@users.noreply.github.com>
2021-06-02 08:10:42 +0530

    Create SwapTwoNumbers.java

---
+++ School level/SwapTwoNumbers.java
@@ -0,0 +1,10 @@
+class Solution{
+    static List<Integer> get(int a,int b)
+    {
+        // code here
+        ArrayList<Integer> a1=new ArrayList<Integer>();
+        a1.add(b);
+        a1.add(a);
+        return a1;
+    }
+}
