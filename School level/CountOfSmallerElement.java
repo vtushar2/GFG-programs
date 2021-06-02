312d1abca934eb5cf36c363fee228c162cec897e
Tushar Verma <55954950+vtushar2@users.noreply.github.com>
2021-06-02 22:52:38 +0530

    Create CountOfSmallerElement.java

---
+++ School level/CountOfSmallerElement.java
@@ -0,0 +1,16 @@
+class Compute {
+    
+    public long countOfElements(long arr[], long n, long x)
+    {
+        int count=0;
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]<=x)
+            {
+                count++;
+            }
+        }
+        return count;
+        
+    }
+}
