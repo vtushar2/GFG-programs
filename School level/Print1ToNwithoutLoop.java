89e0d4e090d094edaede09a47b474b1213c989cb
Tushar Verma <55954950+vtushar2@users.noreply.github.com>
2021-06-03 14:04:09 +0530

    Create Print1ToNwithoutLoop.java

---
+++ School level/Print1ToNwithoutLoop.java
@@ -0,0 +1,13 @@
+class Solution
+{
+    
+  public void printNos(int N)
+    {
+        //Your code here
+        if(N>0)
+        {
+            printNos(N-1);              //recursion
+            System.out.print(N+" ");
+        }
+    }
+}
