f9b9656a2d391e2343d672f67163f42fadf79825
Tushar Verma <55954950+vtushar2@users.noreply.github.com>
2021-06-05 08:54:50 +0530

    Create JavaOverride.java

---
+++ School level/JavaOverride.java
@@ -0,0 +1,12 @@
+class Child extends Parent
+{
+    @Override
+    void show(int p) { System.out.print(p+" "); }
+    
+    @Override
+    void print(int q)
+    {
+        //Add your code here.
+        System.out.println(q*q+" ");
+    }
+}
