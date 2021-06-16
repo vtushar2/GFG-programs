134823ff6db7c4e0a7e69a500442da745576a2cd
Tushar Verma <55954950+vtushar2@users.noreply.github.com>
2021-06-16 22:10:37 +0530

    Create LargestElementInArray.java

---
+++ Basic/LargestElementInArray.java
@@ -0,0 +1,15 @@
+class Compute {
+    
+    public int largest(int arr[], int n)
+    {
+        int max=0;
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]>max)
+            {
+                max=arr[i];
+            }
+        }
+        return max;
+    }
+}
