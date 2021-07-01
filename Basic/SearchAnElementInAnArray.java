8b90ffce88af7512d3c6ccf07fbacea1ccfb7b3d
Tushar Verma <55954950+vtushar2@users.noreply.github.com>
2021-07-01 09:30:48 +0530

    Create SearchAnElementInAnArray.java

---
+++ Basic/SearchAnElementInAnArray.java
@@ -0,0 +1,16 @@
+class Solution{
+        
+    static int search(int arr[], int N, int X)
+    {
+        // Your code here
+        for(int i=0;i<N;i++)
+        {
+            if((X)==arr[i])
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+    
+}
