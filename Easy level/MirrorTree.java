35029774bc4976fce6dc6773d6feb3e13b790d65
Tushar Verma <55954950+vtushar2@users.noreply.github.com>
2021-07-04 09:21:20 +0530

    Create MirrorTree.java

---
+++ Easy level/MirrorTree.java
@@ -0,0 +1,16 @@
+class Solution {
+    // Function to convert a binary tree into its mirror tree.
+    void mirror(Node node) {
+        // Your code here
+        if(node==null)
+        {
+            return;
+        }
+        mirror(node.left);
+        mirror(node.right);
+        
+        Node n=node.left;
+        node.left=node.right;
+        node.right=n;
+    }
+}
