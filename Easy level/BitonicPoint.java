30512523605b61810534f1d74020bfbd9c47b760
Tushar Verma <55954950+vtushar2@users.noreply.github.com>
2021-07-12 14:11:53 +0530

    Create BitonicPoint.java

---
+++ Easy level/BitonicPoint.java
@@ -0,0 +1,14 @@
+class Solution {
+    int findMaximum(int[] arr, int n) {
+        // code here
+        int max=0;
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]>max)
+            {
+                max=arr[i];
+            }
+        }
+        return max;
+    }
+}
