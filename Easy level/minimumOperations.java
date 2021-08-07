546221cf5328afcfa7e50e96ad8d8a5fd84ffb9a
Tushar Verma <55954950+vtushar2@users.noreply.github.com>
2021-08-07 07:56:21 +0530

    Create minimumOperations.java

---
+++ Easy level/minimumOperations.java
@@ -0,0 +1,21 @@
+class Solution
+{
+    public int minOperation(int n)
+    {
+        //code here.
+        int count=0;
+        while(n!=0)
+        {
+            if(n%2==0)
+            {
+                n/=2;
+            }
+            else
+            {
+                n-=1;
+            }
+            count++;
+        }
+        return count;
+    }
+}
